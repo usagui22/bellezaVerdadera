b2aeb3d00e6690fa561d4a15e23c6439f12872f1
usagui22 <danita9516@gmail.com>
2024-09-13 23:59:01 -0400

    navbar basico

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import { Title } from "./Title"
+
+const Navbar = () => {
+  return (
+    <>
+      <Title/>
+      <ul className="contenido_nav">
+        <li>Acerca de</li>
+        <li>Galeria</li>
+        <li>Recomendaciones</li>
+        <li>Contactanos</li>
+      </ul>
+    </>
+  )
+}
+
+export default Navbar
---
+++ src/components/Title.jsx
@@ -0,0 +1,10 @@
+import logoBV from "../assets/Logo.jpg"
+
+export const Title = () => {
+  return (
+    <>
+      <h3>Belleza Verdadera</h3>
+      <img src={logoBV} alt="logoBV"/>
+    </>    
+  )
+}
